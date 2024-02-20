b76ef10824eecd9aaa1f848c96634d65461ef7e6
Nostromo714 <93149393+Nostromo714@users.noreply.github.com>
2024-02-20 15:08:00 -0500

    added notes-contine to work on play round funtion and still need to figure out line 25 error computerSelection/Choice

--- rps.js
+++ rps.js
@@ -3,20 +3,23 @@
       let choice = ["rock", "paper",
       "scissors"];
 
-   
+   //random computer choice //
         let computerSelection = choice[Math.floor(Math.random()*choice.length)];
 
         console.log(computerSelection)
-
+//function that takes computers answer and compares it with players/than delivers message/loop
     function playRound(playerSelection, computerSelection) {
+       
+       
         if (computerSelection = "rock") 
             console.log("its a tie!")
-             if(computerSelection = "paper") 
+        if(computerSelection = "paper") 
             console.log("you win1")
 
             else if("scissors")
-            console.log("you lose paper beats rock")
-            }
+           console.log("you lose paper beats rock")
+           
+         }
       
       const playerSelection = "rock";
       const computerSelection = computerChoice();
