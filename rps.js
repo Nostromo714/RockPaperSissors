2960b31f02bcf52c9ed1c95d5779b6bdacff6126
Nostromo714 <93149393+Nostromo714@users.noreply.github.com>
2024-02-23 15:05:21 -0500

    tested computerSelection if_if_else and else statement is working but i am geting a undifined on row 30

--- rps.js
+++ rps.js
@@ -7,21 +7,25 @@
         let computerSelection = choice[Math.floor(Math.random()*choice.length)];
 
         console.log(computerSelection)
+
 //function that takes computers answer and compares it with players/than delivers message/loop
     function playRound(playerSelection, computerSelection) {
-       
-       
-        if (computerSelection = "rock") 
-            console.log("its a tie!")
-        if(computerSelection = "paper") 
-            console.log("you win1")
 
-            else if("scissors")
-           console.log("you lose paper beats rock")
-           
-         }
-      
-      const playerSelection = "rock";
-      const computerSelection = computerChoice();
+       
+        if (computerSelection === playerSelection){ 
+            console.log("its a tie!!")
+
+        }  else if (computerSelection === "rock" && playerSelection ==="scissors" ||
+            computerSelection === "scissors" && playerSelection ==="paper"|| 
+            computerSelection === "paper" && playerSelection ==="rock")  {
+            console.log("you lose")    
+    } else    
+            //(computerSelection === "paper" && playerSelection === "rock")        
+         console.log("you win!")
+    }
+    
+         
+      const playerSelection = "scissors"
+      //const computerSelection = computerChoice();
       console.log(playRound(playerSelection, computerSelection));
     
