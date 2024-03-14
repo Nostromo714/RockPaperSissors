31b4a3416b2aae5702c5d5e944a1c6460da80d04
Nostromo714 <93149393+Nostromo714@users.noreply.github.com>
2024-03-14 13:43:26 -0400

    added else if statment for plaerselection you win. now else statment returns responce for null

--- rps.js
+++ rps.js
@@ -3,7 +3,13 @@
       let choice = ["rock", "paper",
       "scissors"];
 
+
+
       let playerSelection = prompt("make your choice. Rock, Paper, Scissors")
+
+
+
+
 
    //random computer choice //
         let computerSelection = choice[Math.floor(Math.random()*choice.length)];
@@ -12,18 +18,26 @@
 
 //function that takes computers answer and compares it with players/than delivers message/loop
     function playRound(playerSelection, computerSelection) {
-
-       
+        
         if (computerSelection === playerSelection){ 
             console.log("its a tie!!")
 
         }  else if (computerSelection === "rock" && playerSelection ==="scissors" ||
             computerSelection === "scissors" && playerSelection ==="paper"|| 
-            computerSelection === "paper" && playerSelection ==="rock")  {
-            console.log("you lose")    
-    } else    
-            //(computerSelection === "paper" && playerSelection === "rock")        
+            computerSelection === "paper" && playerSelection ==="rock") { 
+            console.log("you lose") } 
+
+         else if (computerSelection === "scissors" && playerSelection ==="rock" ||
+         computerSelection === "paper" && playerSelection ==="scissors"|| 
+         computerSelection === "rock" && playerSelection ==="paper"){ 
          console.log("you win!")
+         }
+         else {
+            console.log("not a valid choice")
+         }
+            //(computerSelection === "paper" && playerSelection === "rock")        
+         //console.log("you win!")
+
     }
     
          
